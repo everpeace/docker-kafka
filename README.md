c51fb06f554609071a8cdbfe6ab9d3e91bdd745a
Shingo Omura <everpeace@gmail.com>
2016-07-07 17:30:55 +0900

    support to set broker.id by BROKER_ID.

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ To start the image you need to specify a couple of environment variables for the
 | LOG_RETENTION_HOURS | the minimum age of a log file in hours to be eligible for deletion (default is 168, for 1 week) |
 | LOG_RETENTION_BYTES | configure the size at which segments are pruned from the log, (default is 1073741824, for 1GB) |
 | NUM_PARTITIONS| configure the default number of log partitions per topic |
+| BROKER_ID | broker id for the container, if this was unset, unique broker id will be assigned by using zookeeper. |
 
 Ensuring that zk1, zk2 ... zk3 can be resolved is beyond the scope of this image.
 You can use DNS, or Kubernetes services, etc depending on your environment (see below).
--- start-kafka.sh
+++ start-kafka.sh
@@ -6,6 +6,7 @@
 # * LOG_RETENTION_HOURS: the minimum age of a log file in hours to be eligible for deletion (default is 168, for 1 week)
 # * LOG_RETENTION_BYTES: configure the size at which segments are pruned from the log, (default is 1073741824, for 1GB)
 # * NUM_PARTITIONS: configure the default number of log partitions per topic
+# * BROKER_ID: broker id for the container, if this was unset, unique broker id will be assigned by using zookeeper.
 
 # Configure advertised host/port
 if [ ! -z "$ADVERTISED_LISTENERS" ]; then
@@ -39,6 +40,13 @@ fi
 if [ ! -z "$AUTO_CREATE_TOPICS" ]; then
     echo "auto.create.topics.enable: $AUTO_CREATE_TOPICS"
     echo "auto.create.topics.enable=$AUTO_CREATE_TOPICS" >> $KAFKA_HOME/config/server.properties
+fi
+
+if [ ! -z "$BROKER_ID" ]; then
+  echo "broker.id: $BROKER_ID"
+  sed -r -i  "s/broker.id=(.*)/broker.id=$BROKER_ID/g" $KAFKA_HOME/config/server.properties
+else
+  sed -r -i  "s/(broker.id=.*)/#\1/g" $KAFKA_HOME/config/server.properties
 fi
 
 # Run Kafka
